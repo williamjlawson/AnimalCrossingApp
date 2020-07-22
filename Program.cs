06bf9f0c4399fd8aead779c3c7fb46ca3b2b4579
William Lawson git config --global user.name William <wjameslawson@gmail.com>
2020-07-21 20:35:51 -0400

    second commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace AnimalCrossingApp 
+namespace AnimalCrossingApp
 {
     class Program
     {
@@ -14,28 +14,47 @@ namespace AnimalCrossingApp
         {
             //http://acnhapi.com/v1a/villagers/{villagerID}
             Console.WriteLine("Hello World!");
-            var httpClient = new HttpClient();
+            HttpClient httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Accept.Clear();
             httpClient.DefaultRequestHeaders.Add("User-Agent", "Bananas");
-            var stream = httpClient.GetStreamAsync("http://acnhapi.com/v1a/villagers/");
-            var villagers = await JsonSerializer.DeserializeAsync<List<Villager>>(await stream);
+            Task<System.IO.Stream> stream = httpClient.GetStreamAsync("http://acnhapi.com/v1a/villagers/");
+            List<Villager> villagers = await JsonSerializer.DeserializeAsync<List<Villager>>(await stream);
             System.Console.WriteLine(villagers[1].Birthday);
+
+            string userBirthDay ="";
+            string userBirthMonth =""; 
+
+            while (userBirthDay.ToLower().Trim() != "quit")
+            {
+                System.Console.WriteLine("What is the number of the day you were born on? Type quit to exit program");
+                userBirthDay = System.Console.ReadLine();
+
+                if(userBirthDay.ToLower().Trim() == "quit") {
+                    break;
+                }
+                System.Console.WriteLine("What is the number of the month you were born on? Type quit to exit program");
+            }
+
+
+
         }
 
 
     }
 
     // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
-    public class Name    {
+    public class Name
+    {
 
         [JsonPropertyName("name-USen")]
-        public string NameUSen { get; set; }  
+        public string NameUSen { get; set; }
     }
 
-    public class CatchTranslations    {
+    public class CatchTranslations
+    {
 
         [JsonPropertyName("catch-USen")]
-        public string CatchUSen { get; set; } 
+        public string CatchUSen { get; set; }
     }
 
 }
